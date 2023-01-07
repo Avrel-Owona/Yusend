496f541faf91f13c51853a19b2ddb0875126c76c
Avrel-Owona <guyowona01@gmail.com>
2023-01-07 23:25:58 +0100

    nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,16 @@
 const Navbar = () => {
 	return (
-		<nav className="flex justify-between py-5 items-center">
+		<header className="flex justify-between py-5 items-center">
 			<span className="text-gray-50 font-medium text-xl">Yusend</span>
-			<div>
+			<nav>
 				<button className="px-5 mr-4 text-sm py-2 text-gray-400 border-gray-400 bg-slate border rounded-md">
 					Sign in
 				</button>
 				<button className="px-5 text-sm py-2 rounded-md bg-white text-black">
 					Request access
 				</button>
-			</div>
-		</nav>
+			</nav>
+		</header>
 	)
 }
 
