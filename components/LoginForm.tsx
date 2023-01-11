a5377de0623636d03ef29dbb33472cb2424d2e8e
Avrel-Owona <guyowona01@gmail.com>
2023-01-11 14:28:49 +0100

    update

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -2,7 +2,10 @@ import { useForm } from "react-hook-form"
 import { LoginFormType } from "../app/types/auth.type"
 import { loginSchema } from "../app/validations/login.validation"
 import { yupResolver } from "@hookform/resolvers/yup"
+import { useState } from "react"
+
 const LoginForm = () => {
+	const [isLoading, setIsLoading] = useState<boolean>(false)
 	const {
 		register,
 		handleSubmit,
@@ -23,7 +26,7 @@ const LoginForm = () => {
 	return (
 		<form
 			onSubmit={handleSubmit(onLogin)}
-			className={"w-full pt-8 duration-300 ease-in-out"}
+			className={"w-full pt-2 duration-300 ease-in-out"}
 		>
 			<div className="flex flex-col mt-5 text-gray-400 text-sm">
 				<label htmlFor="#" className="mb-2">
@@ -35,7 +38,9 @@ const LoginForm = () => {
 					{...register("email")}
 					className="w-full h-10 px-3 placeholder-gray-500 focus:outline-none bg-input-form rounded-md border border-gray-700"
 				/>
-				<small className="text-red-500 text-xs">{errors.email?.message}</small>
+				<small className="text-red-500 mt-2 text-xs">
+					{errors.email?.message}
+				</small>
 			</div>
 
 			<div className="flex flex-col mt-5 text-gray-400 text-sm">
@@ -48,15 +53,16 @@ const LoginForm = () => {
 					type="password"
 					className="w-full h-10 px-3 placeholder-gray-500 focus:outline-none bg-input-form rounded-md border border-gray-700"
 				/>
-				<small className="text-red-500 text-xs">
+				<small className="text-red-500 mt-2 text-xs">
 					{errors.password?.message}
 				</small>
 			</div>
 			<button
 				className="bg-white font-semibold text-sm text-black w-full mt-10 py-2 h-10 rounded-md"
 				type="submit"
+				disabled={false}
 			>
-				Sign in
+				{isLoading ? "Loading..." : "Login"}
 			</button>
 		</form>
 	)
